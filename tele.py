a014573d4d0246d94be35a558ed70343eed22330
yif <443862442@qq.com>
2024-09-24 20:17:01 +0800

    doc: 调整注释

--- tele.py
+++ tele.py
@@ -1,22 +1,5 @@
 '''
-许大师
-Done! Congratulations on your new bot. You will find it at t.me/MasterXu2004_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-Use this token to access the HTTP API:
-7328937446:AAEphMen-zyNOnAGb_wBmcJVoSAenxGt1ig
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-For a description of the Bot API, see this page: https://core.telegram.org/bots/api
-
-
-===xu da shi
-Done! Congratulations on your new bot. You will find it at t.me/master_xu_2024_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-Use this token to access the HTTP API:
-7922198547:AAE_9oojZhm9lh1gt1wJaZtWRVYFFPCAx0g
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-For a description of the Bot API, see this page: https://core.telegram.org/bots/api
+连接TG自定义机器人
 '''
 
 import urllib.parse
